f4f81ab0adb967dba8eedb12f8874d287bf3ce6b
slee51 <slee51@stevens.edu>
2024-02-20 18:32:03 -0500

    login and and signup basic functions and background

--- app/login/components/sign-in.tsx
+++ app/login/components/sign-in.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Icons } from "@/components/ui/icons";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,49 +11,107 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+const mockUser = {
+  email: "steven@gmail.com",
+  password: "123",
+};
+
+const checkCredentials = (email: string, password: string) => {
+  return email === mockUser.email && password === mockUser.password;
+};
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      if (checkCredentials(email, password)) {
+        console.log("logged in");
+        setEmail("");
+        setPassword("");
+        // navigate to homepage
+      } else {
+        console.log("invalid credentials");
+        setError(true);
+      }
+      console.log("submitted");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <Card>
-      <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl">Welcome Back</CardTitle>
-        <CardDescription>
-          Enter your email below to sign in to your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" />
-        </div>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
+    <form onSubmit={handleSubmit}>
+      <Card>
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl">Welcome Back</CardTitle>
+          <CardDescription className="pb-4">
+            Enter your email below to sign in to your account
+          </CardDescription>
+          {error && (
+            <div
+              className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-2 text-sm"
+              role="alert"
+            >
+              <p className="font-medium">Error logging in</p>
+              <p>Invalid credentials</p>
+            </div>
+          )}
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
           </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
           </div>
-        </div>
-        <div className="grid grid-cols-2 gap-6">
-          <Button variant="outline">
-            <Icons.gitHub className="mr-2 h-4 w-4" />
-            Github
+          <div className="relative my-4">
+            <div className="absolute left-0 right-0 top-1/2 transform -translate-y-1/2">
+              <div className="flex justify-center items-center">
+                <div className="flex-grow border-t border-gray-300"></div>
+                <span className="px-4 bg-white text-xs text-gray-500 uppercase">
+                  Or continue with
+                </span>
+                <div className="flex-grow border-t border-gray-300"></div>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-6">
+            <Button variant="outline">
+              <Icons.gitHub className="mr-2 h-4 w-4" />
+              Github
+            </Button>
+            <Button variant="outline">
+              <Icons.google className="mr-2 h-4 w-4" />
+              Google
+            </Button>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full" type="submit">
+            Sign In
           </Button>
-          <Button variant="outline">
-            <Icons.google className="mr-2 h-4 w-4" />
-            Google
-          </Button>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Sign In</Button>
-      </CardFooter>
-    </Card>
+        </CardFooter>
+      </Card>
+    </form>
   );
 }
