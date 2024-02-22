04b494efb7034fd5f7fce83e39afa29d655fe4cd
Rocco <rvacc@proton.me>
2024-02-22 16:35:32 -0500

    Implemented client-side validation
    Used Zod 🥰 for client side validation and made the error message dynamic to reflect the changes

--- app/login/components/sign-in.tsx
+++ app/login/components/sign-in.tsx
@@ -8,34 +8,47 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+} from "@/components/ui/form";
 import { Icons } from "@/components/ui/icons";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { createClientSupabase } from "@/utils/supabase/client";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+	email: z.string().email().min(1).max(100),
+	password: z.string().min(8).max(100),
+});
 
 export function SignIn() {
 	const supabase = createClientSupabase();
 	const router = useRouter();
 
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [error, setError] = useState(false);
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
 
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+	const handleSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
-			e.preventDefault();
 			const { error } = await supabase.auth.signInWithPassword({
-				email,
-				password,
+				email: values.email,
+				password: values.password,
 			});
 			if (error) {
 				console.error("invalid credentials", error);
-				setError(true);
 			} else {
-				setEmail("");
-				setPassword("");
 				router.push("/"); // navigate to homepage
 			}
 		} catch (error) {
@@ -76,71 +89,96 @@ export function SignIn() {
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<Card>
-				<CardHeader className="space-y-1">
-					<CardTitle className="text-2xl">Welcome Back</CardTitle>
-					<CardDescription className="pb-4">
-						Enter your email below to sign in to your account
-					</CardDescription>
-					{error && (
-						<div
-							className="border-l-4 border-orange-500 bg-orange-100 p-2 text-sm text-orange-700"
-							role="alert"
-						>
-							<p className="font-medium">Error logging in</p>
-							<p>Invalid credentials</p>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(handleSubmit)}>
+				<Card>
+					<CardHeader className="space-y-1">
+						<CardTitle className="text-2xl">Welcome Back</CardTitle>
+						<CardDescription className="pb-4">
+							Enter your email below to sign in to your account
+						</CardDescription>
+						{Object.keys(form.formState.errors).length > 0 && (
+							<div
+								className="border-l-4 border-orange-500 bg-orange-100 p-2 text-sm text-orange-700"
+								role="alert"
+							>
+								<p className="font-medium">
+									Error creating an account
+								</p>
+								{Object.values(form.formState.errors).map(
+									(error, index) => (
+										<p key={index}>{error?.message}</p>
+									),
+								)}
+							</div>
+						)}
+					</CardHeader>
+					<CardContent className="grid gap-4">
+						<div className="grid gap-2">
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Email</FormLabel>
+										<FormControl>
+											<Input
+												placeholder="m@example.com"
+												type="email"
+												{...field}
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
 						</div>
-					)}
-				</CardHeader>
-				<CardContent className="grid gap-4">
-					<div className="grid gap-2">
-						<Label htmlFor="email">Email</Label>
-						<Input
-							id="email"
-							type="email"
-							placeholder="m@example.com"
-							onChange={(e) => setEmail(e.target.value)}
-							value={email}
-						/>
-					</div>
-					<div className="grid gap-2">
-						<Label htmlFor="password">Password</Label>
-						<Input
-							id="password"
-							type="password"
-							onChange={(e) => setPassword(e.target.value)}
-							value={password}
-						/>
-					</div>
-					<div className="relative my-4">
-						<div className="absolute left-0 right-0 top-1/2 -translate-y-1/2 transform">
-							<div className="flex items-center justify-center">
-								<div className="flex-grow border-t border-gray-300"></div>
-								<span className="bg-white px-4 text-xs uppercase text-gray-500">
-									Or continue with
-								</span>
-								<div className="flex-grow border-t border-gray-300"></div>
+						<div className="grid gap-2">
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Password</FormLabel>
+										<FormControl>
+											<Input
+												placeholder="********"
+												type="password"
+												{...field}
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						</div>
+						<div className="relative my-4">
+							<div className="absolute left-0 right-0 top-1/2 -translate-y-1/2 transform">
+								<div className="flex items-center justify-center">
+									<div className="flex-grow border-t border-gray-300"></div>
+									<span className="bg-white px-4 text-xs uppercase text-gray-500">
+										Or continue with
+									</span>
+									<div className="flex-grow border-t border-gray-300"></div>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="grid grid-cols-2 gap-6">
-						<Button variant="outline" onClick={handleGithub}>
-							<Icons.gitHub className="mr-2 h-4 w-4" />
-							Github
+						<div className="grid grid-cols-2 gap-6">
+							<Button variant="outline" onClick={handleGithub}>
+								<Icons.gitHub className="mr-2 h-4 w-4" />
+								Github
+							</Button>
+							<Button variant="outline" onClick={handleGoogle}>
+								<Icons.google className="mr-2 h-4 w-4" />
+								Google
+							</Button>
+						</div>
+					</CardContent>
+					<CardFooter>
+						<Button className="w-full" type="submit">
+							Sign In
 						</Button>
-						<Button variant="outline" onClick={handleGoogle}>
-							<Icons.google className="mr-2 h-4 w-4" />
-							Google
-						</Button>
-					</div>
-				</CardContent>
-				<CardFooter>
-					<Button className="w-full" type="submit">
-						Sign In
-					</Button>
-				</CardFooter>
-			</Card>
-		</form>
+					</CardFooter>
+				</Card>
+			</form>
+		</Form>
 	);
 }
