1d4d07da9b4d1b50aa1616ec4fe0388506611a0a
Justusneumeister <jneumeist@gmail.com>
2024-03-01 16:12:42 -0500

    view-profile mk2

--- app/view-profile/components/view-profile.tsx
+++ app/view-profile/components/view-profile.tsx
@@ -31,16 +31,28 @@ type UserProfile = {
   resume_location: string;
   avatar_location: string;
   email: string;
+  role: string;
+  skills: string[];
+  topSkills: string[];
+  education: string[][];
+  pastProjects: string[][],
+  experience: string[][],
 };
 
 const SampleUserProfile: UserProfile = {
   first_name: "Justus",
   last_name: "Neumeister",
-  bio: "A very cool Guy",
+  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   skill_level: "Intermediate",
   resume_location: "./images/profile.jpg",
   avatar_location: "./images/profile.jpg",
   email: "jneumeis@stevens.edu",
+  role: "Software Engineer",
+  skills: ["C++","C#","Java","SQL"],
+  topSkills: ["C++","C#"],
+  education: [["Steven's Institute of Technology"," - Bachelor's of Science : Computer Science"," : 2020-2024"]],
+  pastProjects: [["ProjPair", " - a very cool and wholesome project"]],
+  experience: [["Meta","- Machine Learning Engineer - ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "]]
 };
 
 export function ViewProfile() {
@@ -48,47 +60,130 @@ export function ViewProfile() {
   const router = useRouter();
 
   return (
-    <Card>
-      <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl">
-          {SampleUserProfile.first_name} {SampleUserProfile.last_name}'s Profile:
-        </CardTitle>
-
-        <CardDescription className="pb-4">
-          Skill Level: {SampleUserProfile.skill_level}
-        </CardDescription>
+    <div>
+    <Card className="flex items-center space-x-8 space-y-1 p-4 mb-8 ">
+      <CardHeader className=" ">
+      <img src={SampleUserProfile.avatar_location} alt="User Image" className="w-25 h-auto mb-4 p-2 rounded-full shadow-md ring-gray-800" />
+       
+        
       </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-2">
-          {/* Add content as needed */}
+      <CardContent className="grid gap-">
+        <div className="flex grid gap-2">
+        <div className="text-lg font-bold">
+          {SampleUserProfile.first_name} {SampleUserProfile.last_name} <br/>
         </div>
-        <div className="grid gap-2">
-          {/* Add content as needed */}
+        <div className="flex items-center gap-2">
+
+        {SampleUserProfile.role}  <div className = "bg-green-400 text-white p-2 rounded-full">
+          {SampleUserProfile.skill_level} 
         </div>
-        <div className="relative my-4">
-          <div className="absolute left-0 right-0 top-1/2 -translate-y-1/2 transform">
-            <div className="flex items-center justify-center">
-              <div className="flex-grow border-t border-gray-300"></div>
-              <span className="bg-white px-4 text-xs uppercase text-gray-500">
-                {SampleUserProfile.first_name}'s Bio:
-              </span>
-              <div className="flex-grow border-t border-gray-300"></div>
-            </div>
+        </div>
+        
+        
+          <br />
+          <br />
+          <div className="text-2x1 font-bold">
+            Top Skills: 
           </div>
+          <br/>
+          <div className="flex flex-wrap gap-2">
+            {SampleUserProfile.topSkills.map((item, index) => (
+            <div key={index} className="bg-green-400 text-white p-2 rounded-full">
+            {item}
+        </div>))}
+      </div>
+          <br/>
+          Skills: <br/>
+          <div className="flex flex-wrap gap-2 max-w-md">
+            {SampleUserProfile.skills.map((item, index) => (
+          <div key={index} className="bg-green-400 text-white p-2 rounded-full">
+            {item}
+          </div>))}
+</div>
+          
         </div>
-        <div className="grid grid-cols-2 gap-6">
-          {/* Display user bio or other relevant information */}
-          {SampleUserProfile.bio}
-        </div>
+   
+        
       </CardContent>
-      <CardFooter>
-        <Button className="w-full" type="submit">
-          Message User
-        </Button>
-		<Button  type="submit">
-          View Resume
-        </Button>
-      </CardFooter>
+      
     </Card>
+
+    <Card className="flex items-center space-x-8 space-y-1 p-4 mb-8 ">
+      <CardHeader className=" ">
+      
+      </CardHeader>
+      <CardContent className="grid gap-">
+        <div className="flex grid gap-2">
+        <div className="text-lg font-bold">
+          About<br/>
+        </div>
+        <div className="max-w-md w-1/2 flex items-center gap-2">
+
+        {SampleUserProfile.bio}  
+       
+        </div>
+        
+        
+          <br />
+          <br />
+          <div className="flex grid gap-2">
+        <div className="text-lg font-bold">
+          Experience<br/>
+        </div>
+        <div className="">
+
+        <div className="flex flex-col gap-2">
+            {SampleUserProfile.experience.map((item, index) => (
+          <div key={index} >
+            {item}
+          
+          </div>))}
+</div>
+       
+        </div>
+        <br /> 
+        <div className="text-lg font-bold">
+          Past Projects<br/>
+        </div>
+        <div className="">
+
+        <div className="flex flex-col gap-2">
+            {SampleUserProfile.pastProjects.map((item, index) => (
+          <div key={index} >
+            {item}
+          
+          </div>))}
+</div>
+       
+        </div>
+
+        <br />
+        <div className="text-lg font-bold">
+          Education<br/>
+        </div>
+        <div className="">
+
+        <div className="flex flex-col gap-2">
+            {SampleUserProfile.education.map((item, index) => (
+          <div key={index} >
+            {item}
+          
+          </div>))}
+</div>
+       
+        </div>
+
+        
+</div>
+          
+        </div>
+   
+        
+      </CardContent>
+      
+    </Card>
+    
+    
+    </div>
   );
 }
--- app/view-profile/page.tsx
+++ app/view-profile/page.tsx
@@ -1,46 +1,21 @@
 import { ViewProfile } from "./components/view-profile";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Sidebar from "../../components/sidebar";
 
 export default function Page() {
   return (
     <div
-      className="flex flex-col h-screen"
+      className="flex "
       style={{ backgroundColor: "#F0F4F7" }}
     >
-      <div className="flex justify-between items-center px-5 py-3 bg-white shadow-sm">
-        <div className="text-3xl">ProgPair</div>
-        <a href="/login">
-          <Button>Sign In</Button>
-        </a>
-      </div>
-      <span className="hidden md:block">
-        <div
-          className="absolute top-0 left-0 h-screen w-3/4 drop-shadow-lg"
-          style={{
-            top: "3.75rem",
-            backgroundColor: "#EEF9FF",
-            height: "calc(100vh - 3.75rem)",
-          }}
-          id="line"
-        ></div>
-        <div
-          className="absolute left-1/3 ml-56 mt-8 w-full aspect-[1] md:max-w-[668px] rounded-full drop-shadow-[3px_0_1px_rgba(0,0,0,0.25)]"
-          style={{ backgroundColor: "#EEF9FF" }}
-          id="circle"
-        ></div>
-        <Image
-          src="/images/profile.jpg"
-          alt="create account background"
-          width={300}
-          height={200}
-          className="absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          style={{ left: "20%" }}
-        />
-      </span>
+      <Sidebar />
 
-      <div className="flex flex-col items-center justify-center h-screen">
-        <div className="w-full max-w-2xl p-8 rounded-lg md:translate-x-1/2 md:-mt-12 md:ml-20 sm: -mt-40">
+        
+
+
+      <div >
+        <div className="w-full p-8 rounded-lg md:translate-x-1/4 md:-mt-4 md:ml-20 sm: -mt-40">
           <ViewProfile />
         </div>
       </div>
