267b73599c6d4bc427bba4727cc277da1055ae0e
taeseoum <20taeseo@gmail.com>
2024-04-15 20:26:06 -0400

    Update page.tsx
    adjusted sidebar

--- app/projects/search/page.tsx
+++ app/projects/search/page.tsx
@@ -3,21 +3,29 @@ import { useState, Dispatch, SetStateAction } from "react";
 import FilterBar from "./components/filter-bar";
 import ProjectCard from "./components/project-card";
 import { FiltersProvider } from "./FiltersContext";
+import Sidebar from "@/components/sidebar";
 
 export default function Page() {
-	return (
-		<FiltersProvider>
-			<div
-				className="md:flex md:flex-col md:items-center min-h-screen"
-				style={{ backgroundColor: "#F0F4F7" }}
-			>
-				<div className="w-full max-w-7xl px-4 pt-4 lg:px-8">
-					<FilterBar />
-				</div>
-				<div className="">
-					<ProjectCard />
-				</div>
-			</div>
-		</FiltersProvider>
-	);
+  return (
+    <FiltersProvider>
+      <div
+        className="md:flex min-h-screen"
+        style={{ backgroundColor: "#F0F4F7" }}
+      >
+        {/* Include the Sidebar component */}
+        <div className="fixed w-64 h-full overflow-y-auto">
+          <Sidebar />
+        </div>
+        
+        <div className="md:flex md:flex-col md:items-center w-full pl-64">
+          <div className="w-full max-w-7xl px-4 pt-4 lg:px-8">
+            <FilterBar />
+          </div>
+          <div>
+            <ProjectCard />
+          </div>
+        </div>
+      </div>
+    </FiltersProvider>
+  );
 }
