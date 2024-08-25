14edd92db5b8de146176f4d0e52f4169c7a5f250
Phillip Anerine <paner225@gmail.com>
2024-08-25 14:55:31 -0400

    example functions

---
+++ app/actions/project.ts
@@ -0,0 +1,39 @@
+"use server";
+import { eq, not } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+import { auth } from "@/db/auth";
+import { db, project } from "@/db/schema";
+import { z } from "zod";
+
+export const projectSchema = z.object({
+	name: z.string().min(3),
+	description: z.string().min(3),
+});
+
+export async function createProject(name: string, description: string) {
+	//TODO: add more fields to the parameters for the stuff in the project table, I just did this as a mockup
+	const session = await auth();
+	if (!session) {
+		return { error: "Not authorized" };
+	}
+	const userId = session.user.id;
+
+	const validatedData = projectSchema.parse({ name, description });
+
+	if (!validatedData) {
+		return { error: "Invalid data" };
+	}
+
+	const insertedProject = await db.insert(project).values({
+		name: validatedData.name,
+		description: validatedData.description,
+		owner_profile_id: userId,
+	});
+
+	await revalidatePath("/api/projects");
+}
+
+export async function getProjects() {
+	const projects = await db.select().from(project).limit(20);
+	return projects;
+}
