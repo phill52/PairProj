b7f5ee6ee0568911e2384bdf801f8d30d225c54c
Jimmy Colgan <jcolgan1@stevens.edu>
2025-11-20 21:29:35 -0500

    progress on migration

--- next/db/auth.ts
+++ next/db/auth.ts
@@ -1,28 +1,26 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import { db } from "./index";
+// import { DrizzleAdapter } from "@auth/drizzle-adapter";
+// import { db } from "./index";
 
-export const {
-	handlers: { GET, POST },
-	auth,
-} = NextAuth({
-	adapter: DrizzleAdapter(db),
-	providers: [GitHub],
-	callbacks: {
-		async session({ session, token, user }) {
-			return session;
-		},
-	},
-});
+// export const {
+// 	handlers: { GET, POST },
+// 	auth,
+// } = NextAuth({
+// 	adapter: DrizzleAdapter(db),
+// 	providers: [GitHub],
+	// callbacks: {
+	// 	async session({ session, token, user }) {
+	// 		return session;
+	// 	},
+// 	},
+// });
 
-/** Not sure if we should add this? **/
-// import NextAuth from "next-auth"
-// import { PrismaAdapter } from "@auth/prisma-adapter"
-// import { prisma } from "./prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./prisma"
  
-// export const { handlers, auth, signIn, signOut } = NextAuth({
-//   adapter: PrismaAdapter(prisma),
-//   providers: [],
-// })
-//taken from https://github.com/fullstackbook/next-auth-drizzle-postgres-tutorial
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub],
+	
+})
--- next/db/prisma.ts
+++ next/db/prisma.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from "../generated/prisma/client"
 import { withAccelerate } from "@prisma/extension-accelerate"
  
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
 export const prisma =
   globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
  
