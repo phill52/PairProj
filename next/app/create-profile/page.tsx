7a71138ba555ecf88b9204cc0f307e90c463945e
Phillip Anerine <paner225@gmail.com>
2026-03-22 19:46:36 -0400

    Disable old drizzle code

--- next/app/create-profile/page.tsx
+++ next/app/create-profile/page.tsx
@@ -1,11 +1,54 @@
 import CreateProfile from "./create-profile";
-import { getCreateProfileProps } from "../actions/user";
+// import { getCreateProfileProps } from "../actions/user";
 import { Suspense } from "react";
+
+async function getCreateProfileProps() {
+	return {
+		profile: {
+			name: "Phill",
+			email: "phill@example.com",
+			image: null,
+			areasOfInterest: ["Frontend"],
+			skills: [
+				{ skillId: "react", skillLevel: "Advanced" },
+				{ skillId: "typescript", skillLevel: "Intermediate" },
+			],
+			education: [
+				{
+					school: "Stevens Institute of Technology",
+					level: "Bachelors of Science",
+					date: "2020-08 - 2024-05",
+					description: "Computer Science",
+				},
+			],
+			experience: [
+				{
+					employer: "Charity Quest",
+					position: "Software Engineer",
+					date: "2021-06 - 2021-08",
+					description: "I worked on the frontend",
+				},
+			],
+		},
+		skills: [
+			{ name: "React", innerColor: "#398100", outerColor: "#D9EAA8" },
+			{
+				name: "TypeScript",
+				innerColor: "#007ACC",
+				outerColor: "#B3D4FC",
+			},
+			{ name: "Python", innerColor: "#7B0D00", outerColor: "#E9B0A9" },
+		],
+		areas: [
+			{ name: "Frontend", innerColor: "#398100", outerColor: "#D9EAA8" },
+			{ name: "Backend", innerColor: "#007ACC", outerColor: "#B3D4FC" },
+			{ name: "DevOps", innerColor: "#7B0D00", outerColor: "#E9B0A9" },
+		],
+	};
+}
 
 async function CreateProfilePage() {
 	const props = await getCreateProfileProps();
-	console.log(props);
-	console.log(props.profile.account_type);
 	return <CreateProfile pageData={props} />;
 }
 
--- next/db/index.ts
+++ next/db/index.ts
@@ -1,24 +1,24 @@
-import { sql } from "drizzle-orm";
-import mysql from "mysql2/promise";
-import { drizzle } from "drizzle-orm/mysql2";
-import { relations } from "drizzle-orm";
-import type { AdapterAccountType } from "next-auth/adapters";
-import dotenv from "dotenv";
-import * as schema from "./schema";
+// import { sql } from "drizzle-orm";
+// import mysql from "mysql2/promise";
+// import { drizzle } from "drizzle-orm/mysql2";
+// import { relations } from "drizzle-orm";
+// import type { AdapterAccountType } from "next-auth/adapters";
+// import dotenv from "dotenv";
+// import * as schema from "./schema";
 
-dotenv.config({
-	path: ".env",
-});
-const connectionString: string = process.env.DATABASE_URL as string;
-if (!connectionString) {
-	throw new Error("DATABASE_URL is not defined");
-}
+// dotenv.config({
+// 	path: ".env",
+// });
+// const connectionString: string = process.env.DATABASE_URL as string;
+// if (!connectionString) {
+// 	throw new Error("DATABASE_URL is not defined");
+// }
 
-let sslConfig = "";
-if (process.env.APP_ENV === "prod") {
-	sslConfig = "?ssl=true";
-}
+// let sslConfig = "";
+// if (process.env.APP_ENV === "prod") {
+// 	sslConfig = "?ssl=true";
+// }
 
-const poolConnection = mysql.createPool(connectionString + sslConfig);
+// const poolConnection = mysql.createPool(connectionString + sslConfig);
 
-export const db = drizzle(poolConnection, { schema, mode: "default" });
+// export const db = drizzle(poolConnection, { schema, mode: "default" });
