985fc160cace3f101ade78d96f29313b06f51b05
Justin Carnemolla <Justincarnemolla123@gmail.com>
2026-04-15 13:28:49 -0400

    clean up projects.ts

--- next/types/projects.ts
+++ next/types/projects.ts
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 export type Project = {
 	id: string;
 	name: string | null;
@@ -14,25 +12,6 @@ export interface Skill {
 	name: string;
 	innerColor: string;
 	outerColor: string;
-=======
-=======
->>>>>>> 05a448d (Improved page & updated to work with new database)
-import { AreaTable, RoleTable, SkillTable } from "./profile-items";
-
-export interface Project {
-	id: string;
-	created_at?: string;
-	name?: string | null;
-	description?: string | null;
-	owner_profile_id?: string | null;
-	skill_level?: string | null;
-	github_repository?: string | null;
-	is_locked?: boolean | null;
-	profile_picture?: string | null;
-<<<<<<< HEAD
->>>>>>> 05a448d (Improved page & updated to work with new database)
-=======
->>>>>>> 05a448d (Improved page & updated to work with new database)
 }
 
 export interface RoleInfo {
@@ -50,7 +29,6 @@ export interface SubmitProject {
 	skills: string[];
 	areasOfInterest: string[];
 	roles: {
-<<<<<<< HEAD
 		name: string;
 		outerColor: string;
 		innerColor: string;
@@ -58,16 +36,6 @@ export interface SubmitProject {
 		optionalSkillIds?: string[];
 		requiredSkillIds?: string[];
 	}[];
-=======
-		[roleName: string]: RoleInfo;
-	};
-
-	skill_level?: string | null;
-	github_repository?: string | null;
-<<<<<<< HEAD
->>>>>>> 05a448d (Improved page & updated to work with new database)
-=======
->>>>>>> 05a448d (Improved page & updated to work with new database)
 }
 
 export type ProjectRoleCreateData = {
